978f5b3b6f1cb251dd6dc046add8b34941fbbba6
leeosha <leeosha53302@gmail.com>
2025-01-23 11:06:59 +0800

    Update hierarchical_pairwise_loss.py

--- hierarchical_pairwise_loss.py
+++ hierarchical_pairwise_loss.py
@@ -3,6 +3,7 @@ def hierarchical_pairwise_loss(pctr_logits, pcvr_logits, click_labels, pay_label
     pctr = tf.nn.sigmoid(pctr_logits)
     pcvr = tf.nn.sigmoid(pcvr_logits)
     pctcvr = pctr * pcvr
+    pctcvr_logit = tf.math.log(pctcvr/(1-pctcvr))
     ########################################
     # 1.pointwise loss
     ########################################
@@ -15,18 +16,29 @@ def hierarchical_pairwise_loss(pctr_logits, pcvr_logits, click_labels, pay_label
     ########################################
     # 2.conditional pairwise loss 
     ########################################
-    pairwise_pctcvr = pctcvr - tf.transpose(pctcvr)
-    price_mask = tf.where(tf.greater(prices - tf.transpose(prices), 0), tf.ones_like(pairwise_pctcvr), tf.zeros_like(pairwise_pctcvr))
-    click_label_mask = tf.where(tf.greater_equal(click_labels - tf.transpose(click_labels), 0), tf.ones_like(pairwise_pctcvr),tf.zeros_like(pairwise_pctcvr))
-    pay_label_mask = tf.where(tf.greater_equal(pay_labels - tf.transpose(pay_labels), 0), tf.ones_like(pairwise_pctcvr),tf.zeros_like(pairwise_pctcvr))
-    mask = pay_label_mask * click_label_mask * price_mask
-    
-    conditional_loss = tf.reduce_mean(tf.losses.log_loss(
+    pairwise_pctcvr = tf.nn.sigmoid(pctcvr_logit - tf.transpose(pctcvr_logit))
+    price_gt_mask = tf.where(tf.greater(prices - tf.transpose(prices), 0), tf.ones_like(pairwise_pctcvr), tf.zeros_like(pairwise_pctcvr))
+    click_label_ge_mask = tf.where(tf.greater_equal(click_labels - tf.transpose(click_labels), 0), tf.ones_like(pairwise_pctcvr),tf.zeros_like(pairwise_pctcvr))
+    pay_label_ge_mask = tf.where(tf.greater_equal(pay_labels - tf.transpose(pay_labels), 0), tf.ones_like(pairwise_pctcvr),tf.zeros_like(pairwise_pctcvr))
+
+    click_label_gt_mask = tf.where(tf.greater(click_labels - tf.transpose(click_labels), 0), tf.ones_like(pairwise_pctcvr),tf.zeros_like(pairwise_pctcvr))
+    click_condition_on_pay_mask = pay_label_ge_mask * click_label_gt_mask
+    click_condition_on_pay_loss = tf.reduce_mean(tf.losses.log_loss(
         predictions=pairwise_pctcvr,
         labels=tf.ones_like(pairwise_pctcvr),
-        weights=mask
+        weights=click_condition_on_pay_mask
     ))
-    conditional_loss = tf.where(tf.is_nan(conditional_loss), tf.zeros_like(conditional_loss), conditional_loss)
+    click_condition_on_pay_loss = tf.where(tf.is_nan(click_condition_on_pay_loss), tf.zeros_like(click_condition_on_pay_loss), click_condition_on_pay_loss)
 
-    hpl_loss = pointwise_loss + conditional_loss
+    
+    price_condition_on_click_pay_mask = pay_label_ge_mask * click_label_ge_mask * price_gt_mask
+    price_condition_on_click_pay_loss = tf.reduce_mean(tf.losses.log_loss(
+        predictions=pairwise_pctcvr,
+        labels=tf.ones_like(pairwise_pctcvr),
+        weights=price_condition_on_click_pay_mask
+    ))
+    price_condition_on_click_pay_loss = tf.where(tf.is_nan(price_condition_on_click_pay_loss), tf.zeros_like(price_condition_on_click_pay_loss), price_condition_on_click_pay_loss)
+
+
+    hpl_loss = pointwise_loss + click_condition_on_pay_loss + price_condition_on_click_pay_loss
     return hpl_loss
