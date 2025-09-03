4f41adc32a672f3a9e8f2f09fe2ae91b92f28a22
leeosha <leeosha53302@gmail.com>
2025-09-03 12:00:30 +0800

    Update hpl.py

--- hpl.py
+++ hpl.py
@@ -19,12 +19,12 @@ def get_loss(pctr_logits, pcvr_logits, click_labels, pay_labels, prices, alpha, 
     # 2.hierarchical pairwise loss 
     ########################################
     pairwise_pctcvr = tf.nn.sigmoid(pctcvr - tf.transpose(pctcvr))
+  
     price_gt_mask = tf.where(tf.greater(prices - tf.transpose(prices), 0), tf.ones_like(pairwise_pctcvr), tf.zeros_like(pairwise_pctcvr))
     click_label_ge_mask = tf.where(tf.greater_equal(click_labels - tf.transpose(click_labels), 0), tf.ones_like(pairwise_pctcvr),tf.zeros_like(pairwise_pctcvr))
     pay_label_ge_mask = tf.where(tf.greater_equal(pay_labels - tf.transpose(pay_labels), 0), tf.ones_like(pairwise_pctcvr),tf.zeros_like(pairwise_pctcvr))
-
-    
     price_hpl_mask = pay_label_ge_mask * click_label_ge_mask * price_gt_mask
+  
     price_hpl_loss = tf.reduce_mean(tf.losses.log_loss(
         predictions=pairwise_pctcvr,
         labels=tf.ones_like(pairwise_pctcvr),
